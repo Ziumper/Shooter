ac63245e7a04190e29360763151a37a8d1c9272e
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-04-13 23:11:24 +0200

    add iweapon interface

---
+++ Assets/Scripts/Ziumper.Shooter/Weapons/IWeapon.cs
@@ -0,0 +1,12 @@
+﻿namespace Ziumper.Shooter.Weapons
+{
+    /**
+     * Generic interface for weapon, because 
+     * everything can be weapon when you want 
+     * for example a pen "Baba jaga" vibes
+     */ 
+    public interface IWeapon
+    {
+        void Use();
+    }
+}
